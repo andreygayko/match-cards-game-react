277ef7769088ca094bad41a6b1aa03187ed788a4
Andrey Gayko <andrey.gayko@gmail.com>
2021-05-21 14:13:38 +0300

    Fix timer format

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -20,7 +20,10 @@ const Timer = () => {
   };
 
   function secondsToString(time: number) {
-    return `${Math.floor(time/3600)}:${Math.floor((time%3600)/60)}:${(time%3600)%60}`
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor(time % 3600 / 60);
+    const seconds = time % 3600 % 60;
+    return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
   };
 
   return (
